2ae320b0ddfb334c8f3b079b8ff068efcb43885d
jorboare <jordi.b.arevalo@gmail.com>
2021-04-22 11:55:59 +0200

    paginas de index y nosotros

--- src/components/contenidoInicio.js
+++ src/components/contenidoInicio.js
@@ -1,36 +1,62 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby'
+import Image from "gatsby-image"
+import styled from '@emotion/styled'
+import { css } from '@emotion/react'
 
-const CotenidoInicio = () => {
+const TextoInicio = styled.div`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+
+    @media(min-width: 768px){
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        column-gap: 100px;
+    }
+
+    p{
+        line-height: 2;
+    }
+`
+const ContenidoInicio = () => {
 
     const informacion = useStaticQuery(graphql`
         query {
 
-allDatoCmsPagina(filter : {slog: {eq: "inicio"}}){
-	nodes {
-		titulo
-    contenido
-    imagen {
-			fluid {
-                ...GatsbyDatoCmsFluid
+            allDatoCmsPagina(filter : {slog: {eq: "inicio"}}){
+	            nodes {
+		            titulo
+                    contenido
+                    imagen {
+			            fluid{
+                            ...GatsbyDatoCmsFluid
+                        }
+                    }
+                }
             }
-    }
-  }
-}
         }
 `)
 
     const { titulo, contenido, imagen } = informacion.allDatoCmsPagina.nodes[0]
+
     return (
         <>
-            <h2>{titulo}</h2>
+            <h2
+                css={css`
+                text-align: center;
+                font-size: 4rem;
+                margin-top: 4rem;
+            `}
+            >{titulo}</h2>
 
-            <div>
+            <TextoInicio>
                 <p>{contenido}</p>
-                <img src={imagen.fluid.src} />
-            </div>
+                <Image fluid={imagen.fluid} alt='habitación hotel' />
+            </TextoInicio>
         </>
     );
 }
 
-export default CotenidoInicio;
+export default ContenidoInicio;
